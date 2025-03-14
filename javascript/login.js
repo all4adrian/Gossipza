e6c51de8b217997e623cc293805564599a888e02
Ani <adrianmilesleeayso@gmail.com>
2025-03-14 11:22:34 +0800

    Add files via upload

---
+++ javascript/login.js
@@ -0,0 +1,43 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMXadXKy0juzqiy9YS-dFV3sqn_cAPvmw",
+  authDomain: "login-example-c78a9.firebaseapp.com",
+  projectId: "login-example-c78a9",
+  storageBucket: "login-example-c78a9.firebasestorage.app",
+  messagingSenderId: "761239243297",
+  appId: "1:761239243297:web:0de068ee26684114c2f160"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app); // <-- This was missing!
+
+// button
+const submit = document.getElementById('submit');
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  // inputs
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      alert("Loggin in...");
+      window.location.href ="game.html"
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+    });
+
+});
